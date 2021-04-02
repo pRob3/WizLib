cf0124f46d829a73853d684d88e407951f8adaf8
pRob3 <hundraprocent@me.com>
2021-04-02 19:58:32 +0200

    Category controller and view

---
+++ WizLib/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Category> objList = db.Categories.ToList();
+
+            return View(objList);
+        }
+    }
+}
