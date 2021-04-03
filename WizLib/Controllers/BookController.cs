e82cb3a93090f52804c0df7da80bbad92db4ed5c
pRob3 <hundraprocent@me.com>
2021-04-03 08:51:31 +0200

    Book controller

---
+++ WizLib/Controllers/BookController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> objList = db.Books.ToList();
+
+            return View(objList);
+        }
+
+        //public IActionResult Upsert(int? id)
+        //{
+        //    Author obj = new Author();
+
+        //    if (id == null) // New
+        //    {
+        //        return View(obj);
+        //    }
+        //    else // Edit
+        //    {
+        //        obj = db.Authors.FirstOrDefault(u => u.Author_Id == id);
+
+        //        if (obj == null)
+        //            return NotFound();
+
+        //        return View(obj);
+        //    }
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(Author obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (obj.Author_Id == 0) // Create
+        //        {
+        //            db.Authors.Add(obj);
+        //        }
+        //        else // Update
+        //        {
+        //            db.Authors.Update(obj);
+        //        }
+        //        db.SaveChanges();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View(obj);
+        //}
+
+        //public IActionResult Delete(int id)
+        //{
+        //    var author = db.Authors.FirstOrDefault(x => x.Author_Id == id);
+        //    if (author != null)
+        //    {
+        //        db.Authors.Remove(author);
+        //        db.SaveChanges();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return NotFound();
+        //}
+
+    }
+}
