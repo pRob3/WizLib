cc242da8f590b69b104685cf49ed7e3f548812c3
pRob3 <hundraprocent@me.com>
2021-04-03 08:12:54 +0200

    Category CRUD

--- WizLib/Controllers/CategoryController.cs
+++ WizLib/Controllers/CategoryController.cs
@@ -24,5 +24,60 @@ namespace WizLib.Controllers
 
             return View(objList);
         }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category obj = new Category();
+
+            if(id == null) // New
+            {
+                return View(obj);
+            }
+            else // Edit
+            {
+                obj = db.Categories.FirstOrDefault(u => u.Category_Id == id);
+
+                if (obj == null)
+                    return NotFound();
+
+                return View(obj);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if(obj.Category_Id == 0) // Create
+                {
+                    db.Categories.Add(obj);
+                }
+                else // Update
+                {
+                    db.Categories.Update(obj);
+                }
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = db.Categories.FirstOrDefault(x => x.Category_Id == id);
+            if(category != null)
+            {
+                db.Categories.Remove(category);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
+            
+            
+        }
     }
 }
